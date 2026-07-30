77e46119ff0ad7b9f3245c1639f7249096f66a84
Samuel <85863952+tancysam@users.noreply.github.com>
2026-07-31 01:58:47 +0800

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,37 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        
+        s1Set = {}
+
+        for ch in s1:
+            s1Set.setdefault(ch, 0)
+            s1Set[ch] += 1
+        
+        window = {}
+        windowLen = 0
+        isPerm = False
+
+        l = 0
+
+        for i,r in enumerate(s2):
+            window.setdefault(r,0)
+            window[r] += 1
+            windowLen += 1
+            if windowLen < len(s1):
+                pass
+            else:
+                if window == s1Set:
+                    isPerm = True
+                else:
+                    window[s2[l]] -= 1
+                    if window[s2[l]] == 0:
+                        window.pop(s2[l])
+                    l += 1
+                    windowLen -= 1
+        return isPerm
+
+
+
+
+
+
