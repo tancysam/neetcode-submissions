73e891819c77f2e1af18b7ea0ffc3a90bd970e8a
Samuel <85863952+tancysam@users.noreply.github.com>
2026-07-31 18:44:33 +0800

    Add: find-target-in-rotated-sorted-array - submission-3

---
+++ Data Structures & Algorithms/find-target-in-rotated-sorted-array/submission-3.py
@@ -0,0 +1,42 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        
+        #Need to reduce list to the portion where target is in
+        # then need to binary search the list for target
+        l = 0
+        r = len(nums) -1
+
+        while l < r:
+            m = (l+r) // 2
+            if nums[m] < nums[r]:
+                r = m
+            else:
+                l = m+1
+    
+
+        numsMin = l
+        l = 0
+        r = len(nums) -1
+
+        if nums[numsMin] <= target <= nums[r]:
+            l = numsMin
+            r = len(nums) -1
+        else: 
+            l = 0
+            r = numsMin -1
+
+        
+        while l<r:
+
+            m = (l+r) //2
+            print(m,nums[m])
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m - 1
+        if nums[l] == target:
+            return l
+        else:
+            return -1
